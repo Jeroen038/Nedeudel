aeb0d43c5b10f6ceb315969ff2b2b6b8dc5e4e7a
Danila_gruznev <302860771@student.rocmondriaan.nl>
2023-10-17 09:47:10 +0200

    game screen added

--- js/main.js
+++ js/main.js
@@ -1,21 +1,25 @@
- // Define an array of element IDs and their corresponding delays
- const elements = [
-    { id: "text", delay: "0.5s" },
-    { id: "text1", delay: "1s" },
-    { id: "text2", delay: "1.5s" },
-    { id: "text3", delay: "2s" },
-  ];
+// Define an array of element IDs and their corresponding delays
+const elements = [
+  { id: "text", delay: "0.5s" },
+  { id: "text1", delay: "1s" },
+  { id: "text2", delay: "1.5s" },
+  { id: "text3", delay: "2s" },
+];
 
-  // Function to set visibility and transition delay for elements
-  function setVisibilityAndDelay(elementId, delay) {
-    const element = document.getElementById(elementId);
-    element.style.visibility = "visible";
-    element.style.transitionDelay = delay;
-  }
+// Function to set visibility and transition delay for elements
+function setVisibilityAndDelay(elementId, delay) {
+  const element = document.getElementById(elementId);
+  element.style.visibility = "visible";
+  element.style.transitionDelay = delay;
+}
 
-  // Call the function for each element on window load
-  window.onload = () => {
-    elements.forEach((element) => {
-      setVisibilityAndDelay(element.id, element.delay);
-    });
-  };
+// Call the function for each element on window load
+window.onload = () => {
+  elements.forEach((element) => {
+    setVisibilityAndDelay(element.id, element.delay);
+  });
+};
+
+setTimeout(function () {
+  window.location.href = "choose.php";
+}, 3000); // Redirect after 5 seconds (adjust as needed)
